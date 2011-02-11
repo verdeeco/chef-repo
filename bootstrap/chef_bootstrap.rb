29b8c62ef5e5dc7eb7e3b4f14120352f4eb1b25e
joewilliams <joe@joetify.com>
2011-02-11 10:40:27 -0800

    change chef_install to ssh in and run commands

--- bootstrap/chef_bootstrap.rb
+++ bootstrap/chef_bootstrap.rb
@@ -1,5 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/ruby
 
+require 'rubygems'
+require 'net/ssh'
+
+commands = <<-EOH
 apt-get update
 apt-get install --force-yes -y ruby rubygems ruby1.8-dev libopenssl-ruby1.8 build-essential curl wget runit
 gem install chef --no-rdoc --no-ri
@@ -15,3 +19,19 @@ chmod 600 /etc/chef/validation.pem
 chef-client
 
 rm /etc/chef/validation.pem
+EOH
+
+if ARGV[2]
+  Net::SSH.start(ARGV[0], ARGV[1], :port => 22, :keys => [ARGV[2]], :paranoid => false ) do |ssh|
+    output = ""
+    commands.each do |command|
+      output = ssh.exec! command
+      puts output
+    end
+  end
+else
+  puts "usage ruby chef_bootstrap.rb hostname user key"
+end
+
+
+
